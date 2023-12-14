157c614bef65685f335bf493a6e5bfa4b3e9121e
Tagaiatto <edilsonmneto@gmail.com>
2023-12-14 19:14:36 -0300

    created SeriesController

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,11 @@
+namespace App\Http\Controllers;
+
+class SeriesController
+{
+    public function listarSeries()
+    {
+        $series = ['One Piece', 'Vox Machina', 'Castlevania'];
+
+        
+    }
+}
